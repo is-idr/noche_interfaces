4ee61e56937c4838d604c226683c0261494bc220
Daniel <dasenovp01@educantabria.es>
2025-12-17 10:31:04 +0100

    Terminado Header

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -1,16 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let hamburger = document.getElementById("hamburger");
-    let icon = hamburger.querySelector(".icon");
-    let content = hamburger.querySelector(".content");
+    const hamburger = document.getElementById("hamburger");
+    const icon = hamburger.querySelector(".icon");
+    const content = hamburger.querySelector(".content");
+    const close = content.querySelector("#close-hamburger-button");
+    const background = hamburger.querySelector("#content-background");
+    background.hidden = true;
     let contentOpened = false;
 
     icon.addEventListener("click", () => {
         contentOpened = !contentOpened;
-        animateContent(content, contentOpened);
+        animateContent(content, background, contentOpened);
+    });
+
+    close.addEventListener("click", () => {
+        contentOpened = false;
+        animateContent(content, background, contentOpened);
+    });
+
+    background.addEventListener("click", () => {
+        contentOpened = false;
+        animateContent(content, background, contentOpened);
     });
 });
 
-function animateContent(content, open) {
+function animateContent(content, background, open) {
     let timing = {
         iterations: 1,
         duration: 350,
@@ -18,7 +31,7 @@ function animateContent(content, open) {
     };
     timing.easing = (open) ? "ease-out" : "ease-in";
 
-    let animation = (open) ? [
+    const contentAnimation = (open) ? [
         { transform: "translateX(100%)" },
         { transform: "translateX(0%)" }
     ] : [
@@ -26,5 +39,17 @@ function animateContent(content, open) {
         { transform: "translateX(100%)" }
     ];
 
-    content.animate(animation, timing);
+    content.animate(contentAnimation, timing);
+
+    const backgroundAnimation = (open) ? [
+        { opacity: "0%" },
+        { opacity: "30%" }
+    ] : [
+        { opacity: "30%" },
+        { opacity: "0%" }
+    ];
+
+    background.animate(backgroundAnimation, timing);
+    if(open) background.hidden = false;
+    else setTimeout(() => { background.hidden = true; }, timing.duration);
 }
